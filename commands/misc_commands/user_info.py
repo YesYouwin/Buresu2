6b6cd8d852c58c7d54d0ba10dff8fed4853f8b97
YesYouwin <garenagaming32@gmail.com>
2026-03-10 12:56:36 +0530

    Update user_info.py

--- commands/misc_commands/user_info.py
+++ commands/misc_commands/user_info.py
@@ -1,4 +1,3 @@
-
 print("Loading UserInfo command")
 
 import discord
@@ -10,14 +9,98 @@ class UserInfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="userinfo", description="Show user info")
+    @app_commands.command(name="userinfo", description="Show detailed user info")
     async def userinfo(self, interaction: discord.Interaction, user: discord.Member):
-        embed = discord.Embed(title=str(user), color=discord.Color.blue())
+
+        if user is None:
+            user = interaction.user
+
+        # Roles
+        roles = [role.mention for role in user.roles if role.name != "@everyone"]
+        role_count = len(roles)
+
+        if len(roles) > 10:
+            role_display = ", ".join(roles[:10])
+        else:
+            role_display = ", ".join(roles)
+
+        if role_display == "":
+            role_display = "None"
+
+        # Boosting
+        boosting = "Yes" if user.premium_since else "No"
+
+        # Administrator permission
+        if user.guild_permissions.administrator:
+            permissions = "👑 Administrator (all permissions)"
+        else:
+            permissions = "Standard Member"
+
+        # Time formatting
+        joined_timestamp = int(user.joined_at.timestamp())
+        created_timestamp = int(user.created_at.timestamp())
+
+        joined_string = f"<t:{joined_timestamp}:f> (<t:{joined_timestamp}:R>)"
+        created_string = f"<t:{created_timestamp}:f> (<t:{created_timestamp}:R>)"
+
+        embed = discord.Embed(
+            title="👥 USER INFORMATION 👥",
+            color=discord.Color.blurple()
+        )
+
         embed.set_thumbnail(url=user.display_avatar.url)
-        embed.add_field(name="User ID", value=user.id)
-        embed.add_field(name="Joined", value=user.joined_at)
+
+        embed.add_field(
+            name="Username",
+            value=f"`{user.name}`",
+            inline=True
+        )
+
+        embed.add_field(
+            name="User ID",
+            value=f"`{user.id}`",
+            inline=True
+        )
+
+        embed.add_field(
+            name=f"Roles [{role_count}] (shows up to 10 roles)",
+            value=role_display,
+            inline=False
+        )
+
+        embed.add_field(
+            name="Nickname",
+            value=f"`{user.nick if user.nick else 'None'}`",
+            inline=True
+        )
+
+        embed.add_field(
+            name="Is boosting",
+            value=f"`{boosting}`",
+            inline=True
+        )
+
+        embed.add_field(
+            name="Global permissions",
+            value=permissions,
+            inline=False
+        )
+
+        embed.add_field(
+            name="Joined this server on (MM/DD/YYYY)",
+            value=f"`{user.joined_at.strftime('%m/%d/%Y %H:%M')}`\n({joined_string})",
+            inline=False
+        )
+
+        embed.add_field(
+            name="Account created on (MM/DD/YYYY)",
+            value=f"`{user.created_at.strftime('%m/%d/%Y %H:%M')}`\n({created_string})",
+            inline=False
+        )
+
         await interaction.response.send_message(embed=embed)
 
 async def setup(bot):
     print("Setting up UserInfo cog")
     await bot.add_cog(UserInfo(bot))
+
